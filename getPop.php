1695b59dfa70b35503db9a64b3f3279182cf069a
Bob Erlenbaugh <bob@erlenbaugh.com>
2014-03-23 20:10:03 -0500

    fixed php ajax

--- getPop.php
+++ getPop.php
@@ -1,15 +1,14 @@
 <?php
-    //echo 'test';
 
-//$slat = intval($_GET['slat']);
+$indexRect = intval($_GET['indexRect']);
+echo $indexRect;
 
 $con = mysqli_connect("127.0.0.1","mapuser","arcabob1","map");
 if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
-//mysqli_select_db($con,"map");
 //$sql="SELECT * FROM testTable WHERE id = '".$q."'";
 $sql="SELECT * FROM testTable;";
 
@@ -17,12 +16,12 @@ $sql="SELECT * FROM testTable;";
 $result = mysqli_query($con,$sql);
 
 while($row = mysqli_fetch_array($result))
-  {
-    
-    echo $row['pop'];
-  
-  }
+{
+
+    $newValue = $row['pop'];
+
+}
 
 mysqli_close($con);
- 
+
 ?>
